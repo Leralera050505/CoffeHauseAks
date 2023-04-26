e27758b7e7a0c50d45021075c7d1365a102bd4a0
Имя Фамилия <example@gmail.com>
2023-04-26 18:38:44 +0300

    te

---
+++ CoffeHause/ClassHelper/Product.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CoffeHause.DB
+{
+    using System;
+    using System.Collections.Generic;
+
+    public partial class Product
+    {
+        public int Quantity { get; set; }
+    }
+}
--- CoffeHause/Windows/BasketWindow.xaml.cs
+++ CoffeHause/Windows/BasketWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Collections.ObjectModel;
+using System.Data.Entity;
 using System.Linq;
 using System.Runtime.Remoting.Contexts;
 using System.Text;
@@ -15,6 +16,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
 using static CoffeHause.ClassHelper.EFclass;
+using static CoffeHause.ClassHelper.BasketClass;
 
 namespace CoffeHause.Windows
 {
@@ -52,6 +54,63 @@ namespace CoffeHause.Windows
                ClassHelper.BasketClass.Products.Remove(selectedProduct);
             }
             GetListProduct();
+        }
+
+        private void BtnAddToCart_Click(object sender, RoutedEventArgs e)
+        {
+            Button button = sender as Button;
+            if (button == null)
+            {
+                return;
+            }
+
+            DB.Product selectedProduct = button.DataContext as DB.Product;
+            if (selectedProduct != null)
+            {
+                selectedProduct.Quantity++;
+                int o = Products.IndexOf(selectedProduct);
+                Products.Remove(selectedProduct);
+                Products.Insert(o, selectedProduct);
+            }
+            GetListProduct();
+        }
+
+        private void AddProduct_Click(object sender, RoutedEventArgs e)
+        {
+            //try
+            //{
+
+            //    DB.SaleProduct Spoduct = new DB.SaleProduct();
+            //    Spoduct.IdProduct = ClassHelper.BasketClass.Products;
+            //    Spoduct.IdSale = 
+            //    check.IDEmployee = ClassHelper.AuthUserClass.authEmploee.IDEmploee;
+            //    check.IDGuest = 1;
+            //    check.Date = DateTime.Now;
+            //    if (check != null)
+            //    {
+            //        Context.Check.Add(check);
+            //        Context.SaveChanges();
+            //    }
+
+
+            //    foreach (var item in stuffsCart)
+            //    {
+            //        DataBase.StuffList stuffList = new DataBase.StuffList();
+            //        stuffList.IDStuff = item.IDStuff;
+            //        stuffList.Quantity = item.Quantity;
+            //        stuffList.IDCheck = Context.Check.ToList().LastOrDefault().IDCheck;
+            //        Context.StuffList.Add(stuffList);
+            //        Context.SaveChanges();
+            //    }
+            //    MessageBox.Show("Продукты успешно добавлены");
+            //    ProductListWindow productListWindow = new ProductListWindow();
+            //    productListWindow.Show();
+            //    Close();
+            //}
+            //catch
+            //{
+            //    MessageBox.Show("Возникла неизвесная ошибка");
+            //}
         }
     }
 }
