74b718d0f2f362f6b7f232ec3805a595612b4dd6
Имя Фамилия <example@gmail.com>
2023-05-01 17:40:11 +0300

    Check

--- CoffeHause/Windows/ReportWindow.xaml.cs
+++ CoffeHause/Windows/ReportWindow.xaml.cs
@@ -23,34 +23,73 @@ namespace CoffeHause.Windows
     /// </summary>
     public partial class ReportWindow : Window
     {
-        string orderby = "Не выбрано";
         List<VW_SaleProduct> saleproduct = new List<VW_SaleProduct>();
         List<string> sortList = new List<string>()
         {
-            "По умолчанию",
-            "По фамилии",
-            "По имени",
-            "По предмету",
-            "По группе"
+            "По работникам",
+            "По клиенту",
+            "По цене",
+            "По количеству"
         };
-        void getDate(DatePicker dateF, DatePicker dateN)
-        {
-            DateO.ItemsSource = ClassHelper.EFclass.Contex.VW_SaleProduct.ToList().Where(i => i.Дата >= dateF.SelectedDate && i.Дата <= dateN.SelectedDate);
-        }
         public ReportWindow()
         {
+
             InitializeComponent();
+            CmbSort.ItemsSource = sortList;
+            CmbSort.SelectedIndex = 0;
+            getDate();
             listviewSale.ItemsSource = Contex.VW_SaleProduct.ToList();
+
         }
+       
+        void getDate()
+        {
+            saleproduct = Contex.VW_SaleProduct.ToList();
+
+            saleproduct = saleproduct.Where(j => j.Цена_покупки.ToString().ToLower().Contains(TxbSearch.Text) || j.Дата.ToString().ToLower().Contains(TxbSearch.Text) ||
+            j.Клиент.ToString().ToLower().Contains(TxbSearch.Text) ||
+            j.Количество.ToString().ToLower().Contains(TxbSearch.Text) ||
+            j.Работник.ToString().ToLower().Contains(TxbSearch.Text)).ToList();
+
+            saleproduct = saleproduct.OrderBy(i => i.Дата).ToList();
+
+
+            switch (CmbSort.SelectedIndex)
+            {
+                case 0:
+                    saleproduct = saleproduct.OrderBy(i => i.Работник).ToList();
+                    break;
+                case 1:
+                    saleproduct = saleproduct.OrderBy(i => i.Клиент).ToList();
+                    break;
+                case 2:
+                    saleproduct = saleproduct.OrderBy(i => i.Цена_покупки).ToList();
+                    break;
+                case 3:
+                    saleproduct = saleproduct.OrderBy(i => i.Количество).ToList();
+                    break;
+                default:
+                    break;
+            }
+
+            listviewSale.ItemsSource = saleproduct;
+        }
+
+
 
         private void CmbSort_SelectionChanged(object sender, SelectionChangedEventArgs e)
         {
-            saleproduct = Contex.VW_SaleProduct.ToList();
+            getDate();
         }
 
         private void TxbSearch_TextChanged(object sender, TextChangedEventArgs e)
         {
-            saleproduct = Contex.VW_SaleProduct.ToList();
+            getDate();
+        }
+
+        private void btnDate_Click(object sender, RoutedEventArgs e)
+        {
+            listviewSale.ItemsSource = Contex.VW_SaleProduct.ToList().Where(i => i.Дата >= dpFirst.SelectedDate && i.Дата <= dpNext.SelectedDate);
         }
     }
 }
